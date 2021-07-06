9ad57d23becf0b28a325dd76fe84d9de09e62b0c
jorgerodriguez1980 <jorgeroqui@hotmail.com>
2021-07-06 18:26:21 -0500

    Java lang lessons

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+
+    private String nombre;
+    private int identificacion;
+    private double estatura;
+    private float peso;
+    private char sexo; //char significa una sola letra
+    private boolean estaVacunado;
+
+    public static void main2(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //operadores aritmeticos
+        int resultado = 45 + 12;
+        resultado = 45 - 12;
+        resultado = 45 * 12;
+        resultado = 45 / 12;
+        resultado = 45 % 12;
+        boolean respuesta = true;
+        //algebra booleana
+        respuesta = 5 < 8;
+        respuesta = 5 > 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 ==8;
+        respuesta = 5 != 8;
+        //operadores logicos
+        respuesta = 5 < 8 && 4 >= 6; //&& se deben cumplir las dos funciones
+        respuesta = 5 < 8 || 4 >= 6; //||or solo se debe cumplir una condicion
+        //estructuras de control
+        if (respuesta) {
+            //bloque de codigo
+        //declaracion de una variable; condicion; incremento    
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+            
+        }
+        
     }
 }
